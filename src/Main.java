925d7214e5861555344e3af678b0763c8df2fe91
Ansar <ansarbaibosinov@gmail.com>
2026-01-09 22:23:53 +0500

    Assignment2

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
 
         Flight f1 = new Flight_type("KC101", "Almaty", 180);
         Flight f2 = new Flight_type("KC102", "Astana", 160);
+        Flight f3 = new Flight_type("KC103", "Madrid", 200);
 
 
         Passenger p1 = new Passenger("Ayan", "P12345");
@@ -22,6 +23,24 @@ public class Main {
 
         b1.printInfo();
         b2.printInfo();
+
+        Flight[] flights = {f1,f2,f3};
+        for (int i = 0; i < flights.length - 1; i++) {
+            for (int j = 0; j < flights.length - 1 - i; j++) {
+                if (flights[j].getCapacity() > flights[j + 1].getCapacity()) {
+                    Flight swap = flights[j];
+                    flights[j] = flights[j + 1];
+                    flights[j + 1] = swap;
+                }
+            }
+        }
+
+        System.out.println("Capacity: ");
+        for(int i = 0;i< flights.length;i++){
+            flights[i].printInfo();
+        }
+
+
 
 
 
